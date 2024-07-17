ba79b0dc5a6cdf8c6e489acb0a0bf804cb93e41f
NikkyDark <bobachev_nik@mail.ru>
2024-07-17 23:40:56 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,20 @@
+my_string = input("как тебя зовут? ")
+print(my_string)
+#  тут не много не понял, про вывести количество символов.
+
+my_string = input("как тебя зовут? ")
+print("Привет! ", my_string.upper())
+#
+my_string = input("как тебя зовут? ")
+print("Привет! ", my_string.lower())
+
+my_string = input("как тебя зовут? ")
+print("Привет! ", my_string.replace(' ', '#'))
+
+my_string = input("как тебя зовут? ")
+print("Привет! ", my_string[0])
+
+
+my_string = input("как тебя зовут? ")
+print("Привет! ", my_string)
+print(my_string[-1])
