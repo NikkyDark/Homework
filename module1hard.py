799176ca01ea031ed1bd73aad2137c308d7a2941
NikkyDark <bobachev_nik@mail.ru>
2024-07-21 21:35:26 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,8 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Jhonny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+a = average_1 = sum(grades[0]) / len(grades[0])
+b = average_2 = sum(grades[1]) / len(grades[1])
+c = average_3 = sum(grades[2]) / len(grades[2])
+d = average_4 = sum(grades[3]) / len(grades[3])
+e = average_5 = sum(grades[4]) / len(grades[4])
+print({'Jhonny': a,'Bilbo': b, 'Steve': c, 'Khendrik': d, 'Aaron': e})
