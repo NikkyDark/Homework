6e7baa756ee2bb05d2c0a68b858fef22d9cb59b5
NikkyDark <bobachev_nik@mail.ru>
2024-07-21 23:57:48 +0300

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -1,7 +1,8 @@
 my_dict = {'Nikita': 1993, 'Mary': 1995}
-print(my_dict['Mary'])
+print(my_dict)
+
 my_dict['Victory'] = 2003
-print(my_dict['Victory'])
+print(my_dict['Victory'], my_dict['Mary'])
 my_dict.update({'Sobaka': 2018,
                'Sobaka_2': 2020})
 print(my_dict)
@@ -12,3 +13,5 @@ my_set = {1, 'лето', 'зима', 2, 1, 2, (2, 1.5)}
 my_set.add(5), my_set.add(7)
 my_set.discard(1)
 print(my_set)
+
+
