42daa62f737d0fdef3d8ad6d73d747ae91fe5441
NikkyDark <bobachev_nik@mail.ru>
2024-07-23 09:54:45 +0300

    Update module_2.2.py

--- module_2.2.py
+++ module_2.2.py
@@ -5,5 +5,5 @@ if first == second == third:
     print(3)
 elif first == second or first == third or second == third:
     print(2)
-else first != second != third:
+elif first != second != third:
     print(0)
