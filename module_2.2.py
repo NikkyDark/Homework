b836b3677266e1902a4c4f4c9b11760a0b828ed4
NikkyDark <bobachev_nik@mail.ru>
2024-07-23 07:36:07 +0300

    Update module_2.2.py

--- module_2.2.py
+++ module_2.2.py
@@ -1,9 +1,9 @@
 first = int(input('Введите число: '))
 second = int(input('Введите число: '))
 third = int(input('Введите число: '))
-if first == second and first == second and first == third:
+if first == second == third:
     print(3)
-elif first == second or first == third:
+elif first == second or first == third or second == third:
     print(2)
-elif first != second and first != third and first != third:
+else first != second != third:
     print(0)
