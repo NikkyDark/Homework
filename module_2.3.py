144f4f22bb617e5d52414372f8e967b414200721
NikkyDark <bobachev_nik@mail.ru>
2024-07-26 20:58:40 +0300

    Update module_2.3.py

--- module_2.3.py
+++ module_2.3.py
@@ -7,9 +7,6 @@ while i <= len(my_list):
     if my_list[i] == 0:
         i += 1
         continue
-    if my_list[i] < 0:
-        break
     if my_list[i] > 0:
         print(my_list[i])
     i += 1
-
