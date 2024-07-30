36f8e771fa11f23b417049c798dc576dfb684301
NikkyDark <bobachev_nik@mail.ru>
2024-07-30 09:44:59 +0300

    Update module_2.5.py

--- module_2.5.py
+++ module_2.5.py
@@ -1,11 +1,10 @@
 def get_matrix(n, m, value):
-    line = [value] * m
-    matrix = [line] * n
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
     return matrix
-
-
 print(get_matrix(2, 2, 10))
 print(get_matrix(3, 5, 42))
-print(get_matrix(4, 2, 13))
-
-
+print(get_matrix(2, 4, 13))
