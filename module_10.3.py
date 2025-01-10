926740a83585a02e85253320b748d3a531c3032c
Nikita Dark <bobachev_nik@mail.ru>
2025-01-10 23:20:55 +0300

    Update module_10.3.py

--- module_10.3.py
+++ module_10.3.py
@@ -1,5 +1,5 @@
-import random
 import threading
+import random
 import time
 
 
@@ -10,38 +10,37 @@ class Bank:
 
     def deposit(self):
         for _ in range(100):
-            amount = random.randint(50, 100)
-        with self.lock:
-            self.balance += amount
-        print(f'Пополнение :{amount}. Баланс: {self.balance}.')
-        if self.balance >= 500 and self.lock.locked():
-            self.lock.release()
-
-    time.sleep(0.001)
+            amount = random.randint(50, 500)
+            with self.lock:
+                self.balance += amount
+                print(f"Пополнение: {amount}. Баланс: {self.balance}")
+                if self.balance >= 500 and not self.lock.locked():
+                    self.lock.release()
+            time.sleep(0.001)
 
     def take(self):
         for _ in range(100):
-            amount = random.randint(50, 100)
-            print(f'Запрос на {amount}')
-            with self.lock:
+            amount = random.randint(50, 500)
+            print(f"Запрос на {amount}")
+            with self.lock:  # 
                 if amount <= self.balance:
-                    print(f'Снятие: {amount}. Баланс: {self.balance}.')
+                    self.balance -= amount
+                    print(f"Снятие: {amount}. Баланс: {self.balance}")
                 else:
-                    print(f'Запрос отклонен, недостаточно средств')
-                self.lock.acquire()
-
+                    print("Запрос отклонён, недостаточно средств")
+                    self.lock.acquire()
             time.sleep(0.001)
 
 
-bank = Bank()
+bk = Bank()
 
-deposit_thread = threading.Thread(target=bank.deposit)
-take_thread = threading.Thread(target=bank.take)
+th1 = threading.Thread(target=bk.deposit)
+th2 = threading.Thread(target=bk.take)
 
-deposit_thread.start()
-take_thread.start()
+th1.start()
+th2.start()
 
-deposit_thread.join()
-take_thread.join()
+th1.join()
+th2.join()
 
-print(f"Итоговый баланс: {bank.balance}")
+print(f"Итоговый баланс: {bk.balance}")
