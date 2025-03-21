656eb6db8077dd0b3f90a44004773d5dc56392eb
Nikita Dark <bobachev_nik@mail.ru>
2025-03-21 09:16:19 +0300

    Update module_13.6.py

--- module_13.6.py
+++ module_13.6.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-api = '8154316130:AAFABEUwpWCyXyv7XVp9tCnvHKzuaZZ-Rks'
+api = 'Введите ваш токен:'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
